854ddb5059b26d5cfdb2761df4f4870fc137cf87
lexilya <lexilya@users.noreply.github.com>
2018-02-26 10:39:52 +0100

    Use setasign\Fpdi instead in PDFMerger.

--- src/PDFMerger/PDFMerger.php
+++ src/PDFMerger/PDFMerger.php
@@ -24,8 +24,7 @@
 namespace Clegginabox\PDFMerger;
 
 use Exception;
-use fpdi\FPDI;
-use fpdf\FPDF;
+use setasign\Fpdi\Fpdi;
 
 class PDFMerger
 {
@@ -66,7 +65,7 @@ class PDFMerger
             throw new Exception("No PDFs to merge.");
         }
 
-        $fpdi = new FPDI;
+        $fpdi = new Fpdi;
 
         // merger operations
         foreach ($this->_files as $file) {
@@ -82,7 +81,7 @@ class PDFMerger
                     $template   = $fpdi->importPage($i);
                     $size       = $fpdi->getTemplateSize($template);
 
-                    $fpdi->AddPage($fileorientation, array($size['w'], $size['h']));
+                    $fpdi->AddPage($fileorientation, array($size['width'], $size['height']));
                     $fpdi->useTemplate($template);
                 }
             } else {
@@ -92,7 +91,7 @@ class PDFMerger
                     }
                     $size = $fpdi->getTemplateSize($template);
 
-                    $fpdi->AddPage($fileorientation, array($size['w'], $size['h']));
+                    $fpdi->AddPage($fileorientation, array($size['width'], $size['height']));
                     $fpdi->useTemplate($template);
                 }
             }
@@ -102,16 +101,15 @@ class PDFMerger
         $mode = $this->_switchmode($outputmode);
 
         if ($mode == 'S') {
-            return $fpdi->Output($outputpath, 'S');
+            return $fpdi->Output('S', $outputpath);
         } else {
-            if ($fpdi->Output($outputpath, $mode) == '') {
+            if ($fpdi->Output($mode, $outputpath) == '') {
                 return true;
             } else {
                 throw new Exception("Error outputting PDF to '$outputmode'.");
                 return false;
             }
         }
-
 
     }
 
